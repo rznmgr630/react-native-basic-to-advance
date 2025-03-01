743a41e2b7de60a7cddbe5a1fed9ba5e055990a0
rznmgr630 <rajan.midun1@gmail.com>
2025-03-01 12:00:16 +0545

    feat: used the router from the expo-router for navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,13 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
+
 import FileLogo from "@/assets/images/post-it.png";
 
 const HomeScreen = () => {
+  const router = useRouter();
+
+  const handleClick = () => router.push("/notes");
+
   return (
     <View style={styles.container}>
       <Image source={FileLogo} style={styles.image} />
@@ -9,6 +15,9 @@ const HomeScreen = () => {
       <Text style={styles.subtitle}>
         Capture your thoughts anytime, anywhere
       </Text>
+      <TouchableOpacity style={styles.btn} onPress={handleClick}>
+        <Text style={styles.btnText}>Get Started</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -36,6 +45,18 @@ const styles = StyleSheet.create({
     color: "#666",
     textAlign: "center",
     marginBottom: 20,
+  },
+  btn: {
+    backgroundColor: "#007bff",
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  btnText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
 
