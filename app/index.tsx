febe3037fd3192fd8b3ea4e356072f5fdcc40889
rznmgr630 <rajan.midun1@gmail.com>
2025-03-01 11:45:11 +0545

    feat: adding basic styles using the Stylesheet component

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,34 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Rajan's Notes App</Text>
+      <Text style={styles.subtitle}>
+        Capture your thoughts anytime, anywhere
+      </Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#333",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#666",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+});
 
 export default HomeScreen;
