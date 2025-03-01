81af06308aa2dedf7049bd1940fa35adcc87852f
rznmgr630 <rajan.midun1@gmail.com>
2025-03-01 12:15:53 +0545

    feat: using flatlist to display the list of notes

--- app/notes/index.tsx
+++ app/notes/index.tsx
@@ -1,9 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 const NoteScreen = () => {
+  const [notes, setNotes] = useState([
+    { id: 1, title: "Note 1" },
+    { id: 2, title: "Note 2" },
+    { id: 3, title: "Note 3" },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text> Note Page</Text>
+      <FlatList
+        data={notes}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.noteItem}>
+            <Text style={styles.noteText}>{item.title}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -11,7 +26,18 @@ const NoteScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
+    padding: 20,
+  },
+  noteItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#f5f5f5",
+    padding: 15,
+    borderRadius: 5,
+    marginVertical: 5,
+  },
+  noteText: {
+    fontSize: 18,
   },
 });
 
